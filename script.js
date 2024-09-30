4ad2cd6f29a2a6a70881ada031492d78d12b4c4a
Marcelacaf <caf.marcela@gmail.com>
2024-09-30 17:44:30 -0300

    agregué más funcionalidades en JS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const itemInput = document.getElementById('item-input');
 const itemForm = document.getElementById('item-form');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 itemForm.addEventListener('submit', function (e){
   e.preventDefault();
@@ -25,5 +28,40 @@ itemForm.addEventListener('submit', function (e){
   newItem.textContent = itemValue;
   newItem.appendChild(redBtn);
 
+  //Add li to the DOM
   itemList.appendChild(newItem);
+  checkUI();
+
+  itemInput.value = ' ';
 });
+
+
+function removeItem(e){
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Estás segur@?')){
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function clearItems() { 
+  while (itemList.firstChild){
+    itemList.removeChild(itemList.firstChild);
+    checkUI();
+  } 
+}
+
+function checkUI(){
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0){
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  }
+}
+
+
+//Events Listeners
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+checkUI();
